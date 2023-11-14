2925d7387a136795cba7a49b47be7b73f1e560d1
MICastilloHurtado <122422130+MICastilloHurtado@users.noreply.github.com>
2023-11-13 20:52:11 -0500

    first commit

---
+++ src/main/java/com/mycompany/pokegame/PokeGame.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.pokegame;
+
+public class PokeGame {
+
+    public static void main(String[] args) {
+        Picachu pika = new Picachu();
+        Charmander charman = new Charmander();
+        Squirtle squirt = new Squirtle();
+        Bulbasur bulba = new Bulbasur();
+        
+        pika.atacarArañazo();
+        charman.atacarPunioFuego();
+    }
+}
